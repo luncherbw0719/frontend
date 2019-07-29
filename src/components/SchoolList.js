29d7eb38af634c3df9e44d0e1bcb44d2e997e426
Reese Kunz <reesekunz21@gmail.com>
2019-07-29 15:13:26 -0700

    set up school list component, styled card

--- src/components/SchoolList.js
+++ src/components/SchoolList.js
@@ -1,8 +1,32 @@
-import React, { useEffect, useState } from "react";
-//import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-function SchoolList() {
-  return <div />;
+import axios from "axios";
+
+import SchoolCard from "./SchoolCard";
+
+function SchoolList(props) {
+  const [schools, setSchools] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://schooldonations-luncher.herokuapp.com/schools/schools")
+      .then(response => {
+        console.log("Component mounted, data = ", response.data.results);
+
+        setSchools(response.data.results);
+      })
+
+      .catch(error => {
+        console.error("no bueno", error);
+      });
+  }, []);
+  return (
+    <section className="school-list">
+      {schools.map(school => {
+        return <SchoolCard key={school.id} {...school} />;
+      })}
+    </section>
+  );
 }
 
 export default SchoolList;
