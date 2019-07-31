5d341f0f334df4094fe72d6222c7a4b1b093b35d
Margaret <mpbeck@umich.edu>
2019-07-31 16:01:06 -0400

    cardstyling

--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -4,11 +4,11 @@ import './styles/SchoolCard.scss';
 const SchoolCard = props => {
   return (
     <div className='school-card'>
-      <h1>School:{props.schoolName}</h1>
-      <h2>School ID:{props.schoolID}</h2>
-      <h3>Location:{props.schoolLocation}</h3>
-      <h3>Current Funds:{props.CurrentFunds}</h3>
-      <h3>Funds Needed:{props.NeededFunds}</h3>
+      <h1>School: {props.schoolName}</h1>
+      <h2>School ID: {props.schoolID}</h2>
+      <h3>Location: {props.schoolLocation}</h3>
+      <h3>Current Funds: {props.CurrentFunds}</h3>
+      <h3>Funds Needed: {props.NeededFunds}</h3>
     </div>
   );
 };
