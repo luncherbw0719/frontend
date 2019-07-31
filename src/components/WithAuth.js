961cd8191ceb3cdde4ddc33c9d1d64caafffcef3
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-07-31 15:14:30 -0700

    styled login page

--- src/components/WithAuth.js
+++ src/components/WithAuth.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 import { login } from "../actions";
 
+import './styles/WithAuth.scss';
+
 export const withAuth = Component =>
   connect(
     state => ({ ...state }),
@@ -55,15 +57,25 @@ export const withAuth = Component =>
       <div className="signup">Sign up page</div>
     ) : (
       <form onSubmit={login} className="login">
+        <h1>Log in</h1>
         <div className='form-field'>
           <label className='field-label'>E-mail</label>
-          <input disabled={props.loggingIn} className='form-input' name='email' onChange={handleChange} type="text" />
+          <input disabled={props.loggingIn} className='field-input' name='email' onChange={handleChange} type="text" />
         </div>
         <div className='form-field'>
           <label className='field-label'>Password</label>
-          <input disabled={props.loggingIn} className='form-input' name='password' onChange={handleChange} type="password" />
+          <input disabled={props.loggingIn} className='field-input' name='password' onChange={handleChange} type="password" />
         </div>
         <button disabled={props.loggingIn} className='form-btn' type="submit">Login</button>
+        {form.error && <div className='error'>
+          {form.error}
+        </div>}
+        {props.loginError && <div className='error'>
+          {props.loginError}
+        </div>}
+        <div className='switch'>
+          Don't have an account? <span className='signup-btn'>Create one here</span>!
+        </div>
       </form>
     );
   });
