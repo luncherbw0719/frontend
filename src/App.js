e106468b3a6ba5297c1c4cf19d0e575f1809bd48
Reese Kunz <reesekunz21@gmail.com>
2019-07-31 15:38:53 -0700

    added donor card for schools

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import SchoolPage from './components/SchoolPage';
 
 import { Route } from 'react-router-dom';
 
+import SchoolDonor from './components/SchoolDonor';
+
 function App(props) {
   const init = () => {
     props.setToken(localStorage.getItem('token'));
@@ -23,7 +25,7 @@ function App(props) {
     name: "Woodbridge",
     location: "Irvine",
     currentFunds: 1000,
-    neededFunds: 400,
+    fundGoal: 4000,
     schoolId: 0,
     donors: []
   }
@@ -33,7 +35,7 @@ function App(props) {
       <Navbar />
       <Route exact path='/' render={props => <Landing {...props} />} />
       <Route path='/schools' render={props => <SchoolBrowse {...props} />} />
-      <Route path='/sdashboard' render={props => <SchoolPage {...props} school={fakeSchool} />} />
+      <Route path='/sdashboard' render={props => <SchoolPage {...props} dashboard school={fakeSchool} />} />
       {/* Add more routes above this comment as necessary */}
     </div>
   );
