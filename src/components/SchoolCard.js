3a345e986dbf18bd127ef17fb0f75eca3d7bcd03
Margaret <mpbeck@umich.edu>
2019-07-31 15:49:55 -0400

    new card and beginning stylings

--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
 import './styles/SchoolCard.scss';
 
 const SchoolCard = props => {
   return (
     <div className='school-card'>
-      School information goes in here!
+      <h1>School:{props.schoolName}</h1>
+      <h2>School ID:{props.schoolID}</h2>
+      <h3>Location:{props.schoolLocation}</h3>
+      <h3>Current Funds:{props.CurrentFunds}</h3>
+      <h3>Funds Needed:{props.NeededFunds}</h3>
     </div>
   );
 };
